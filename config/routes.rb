d2041a34d50d4d5dfbd6b123c2f4844dfd3526ca
Javad Effat Doost <j.effatdoost@gmail.com>
2025-12-16 09:01:13 +0330

    preparing for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "/projects", to: "pages#projects", as: "projects"
+  get "/blog", to: "pages#blog", as: "blog"
+  get "/about", to: "pages#about", as: "about"
+  get "/contact", to: "pages#contact", as: "contact"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
